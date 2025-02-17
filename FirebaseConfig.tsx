2c829e76682656060f8f4da407f32208b86bdc89
Ehtisham-rn <155835266+Ehtisham-rn@users.noreply.github.com>
2025-02-17 14:26:50 +0500

    Update FirebaseConfig.tsx

--- FirebaseConfig.tsx
+++ FirebaseConfig.tsx
@@ -6,13 +6,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7eXFp1NoYkcAj0t8MbTSX8X-ew_eFXeo",
-  authDomain: "unilink-72197.firebaseapp.com",
-  projectId: "unilink-72197",
-  storageBucket: "unilink-72197.appspot.com",
-  messagingSenderId: "48354191648",
-  appId: "1:48354191648:web:3941a10274a1e6a5584f45",
-  measurementId: "G-0JGNEXPR2F",
+
 };
 
 // Initialize Firebase
